c01258e20fc8f9594275dc637ebad2121af5eda3
Axect <axect@outlook.kr>
2022-07-08 13:57:03 +0900

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,93 @@
+use serde_derive::Deserialize;
+use std::fs::read_to_string;
+use std::fs;
+use indoc::formatdoc;
+
+fn main() {
+    let config = Template::parse_toml("template.toml");
+    println!("{:?}", config);
+
+    let yaml = formatdoc! {"
+        ---
+        session:
+          name: \"{name}\"
+          attach: true
+        template:
+          direction: Horizontal
+          parts:
+            - direction: Vertical  # part 1
+              borderless: true
+              split_size:
+                Fixed: 1
+              run:
+                plugin:
+                  location: \"zellij:tab-bar\"
+            - direction: Vertical # part 2
+              body: true
+            - direction: Vertical # part 3
+              borderless: true
+              split_size:
+                Fixed: 2
+              run:
+                plugin:
+                    location: \"zellij:status-bar\"
+        tabs:
+          - name: \"work\"
+            focus: true
+            direction: Vertical
+            parts:
+              - direction: Horizontal
+                run:
+                  command: {{ cmd: zsh, args: [\"-c\", \"cd {dir} && nvim\"] }}
+                focus: true
+              - direction: Horizontal
+                parts:
+                  - direction: Vertical
+                    run:
+                      command: {{ cmd: zsh, args: [\"-c\", \"cd {dir} && broot\"] }}
+                  - direction: Vertical
+                    run:
+                      command: {{ cmd: zsh, args: [\"-c\", \"cd {dir} && zsh\"] }}
+          - name: \"perf\"
+            direction: Vertical
+            run:
+              command: {{ cmd: btm }}
+          - name: \"git\"
+            direction: Vertical
+            run:
+              command: {{ cmd: zsh, args: [\"-c\", \"cd {dir} && gitui\"] }}
+        ",
+        name = config.get_name(),
+        dir = config.get_dir(),
+    };
+
+    let yaml_file_name = format!("{}.yaml", config.get_name());
+    let yaml_file = fs::File::create(&yaml_file_name).unwrap();
+    fs::write(yaml_file_name, yaml).unwrap();
+}
+
+#[derive(Debug, Deserialize)]
+struct Template {
+    config: Config
+}
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    name: String,
+    dir: String
+}
+
+impl Template {
+    fn parse_toml(toml_path: &str) -> Self {
+        let config = read_to_string(toml_path).expect("Can't read config.toml");
+        toml::from_str(&config).expect("Can't parse config.toml")
+    }
+
+    fn get_name(&self) -> &str {
+        &self.config.name
+    }
+
+    fn get_dir(&self) -> &str {
+        &self.config.dir
+    }
+}
